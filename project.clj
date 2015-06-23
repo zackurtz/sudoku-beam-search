ae150fead38385024652fbf92bc22ce4876f78eb
Zachary Kurtz <zac.kurtz@gmail.com>
2015-06-22 23:40:54 -0400

    Fixed license and readme

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject sudoku-beam-search "1.0"
   :description "Sodoku Solver using Beam Search"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License (MIT)"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot sudoku-beam-search.core
   :target-path "target/%s"
