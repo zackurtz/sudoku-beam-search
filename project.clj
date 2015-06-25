01e3f06c9eb8dc8685ff459efb4da3f3bb3d1c80
Zachary Kurtz <zac.kurtz@gmail.com>
2015-06-24 23:30:42 -0400

    Fixed up loops. Added math library for floor.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sudoku-beam-search "1.0"
   :description "Sodoku Solver using Beam Search"
   :license {:name "MIT License (MIT)"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot sudoku-beam-search.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
