fcfb9fb7db86ca1a74c35512aaca5eeb26163b5b
Zachary Kurtz <zac.kurtz@gmail.com>
2015-06-26 23:42:03 -0400

    Almost done with the rough port. One last tricky return-from function.

--- src/sudoku_beam_search/solver.clj
+++ src/sudoku_beam_search/solver.clj
@@ -54,10 +54,7 @@
 (defn find-moves [grid]
   (let [dim (count grid) temp-valid nil]
     (for [row (range 0 (nth 0 dim)) col (range 0 (nth 1 dim)) :when (empty-cell (get-in grid [row col]))]
-        (do
-          (set temp-valid (list-valid grid row col))
-          (if (nil? temp-valid) (return nil))
-          (list (list row col) temp-valid))))))
+        '('(row col) (list-valid grid row col)))))
 
 
 ;; My thought is that the search can be designed to not explore invalid states,
@@ -65,52 +62,48 @@
 ;; state
 ;; is-full is the goal test in my search
 (defn is-full [grid]
-  (let ((dim (array-dimensions grid)) )
+  (let [dim (count grid)]
     (= 0 (count (take 1 (for [row (range 0 (nth 0 dim)) col (range 0 (nth 1 dim))
                   :when (empty-cell (get-in grid [row col]))]
       'empty))))))
 
-;; Generates a new state based on a state and a move
+;; Generates a new states based on a state and a move
 (defn apply-move [grid move]
-  (let ((row (car (car move))) (col (cadr (car move))) (values (cadr move))
-    (temp nil))
-    (for [value values] :collect
-      (do
-        (setf temp (COPY-ARRAY grid))
-        (setf (aref temp row col) value)
-        (list temp (length values))))))
+  (let [row (first (first move)) col (fnext (first move)) values (fnext move)]
+    (for [value values]
+        '(assoc grid row (assoc (grid row) col value) (count values)))))
 
 
 ;; Generates all new states from one state
 (defn successor-states [grid]
-  (let ((moves (find-moves grid)))
-    (for [move moves] :append
+  (let [moves (find-moves grid)]
+    (for [move moves]
         (apply-move grid move))))
 
 ;; Generates all successor games from the given game, checking for and removing
 ;; duplicate states
 ;; Correctly sets the value of sibling-games
 (defn expand-game-best [game closed]
-  (let ((boards (successor-states (game-board game))))
-    (loop for board in boards collect
-        (if (not (gethash (prin1-to-string (car board)) closed))
+  (let [boards (successor-states game)]
+    (for [board boards]
+        (if (not (get closed (first board)))
         (do
-          (setf (gethash (prin1-to-string (car board)) closed) t)
-               { :name (game-name game)
-                 :board (car board) :parent game
-                 :depth (+ (game-depth game) 1)
-                 :siblings (cadr board) } ))))))
+          (set (get closed (first board)) true)
+               { :name (:name game)
+                 :board (first board) :parent game
+                 :depth (+ (:depth game) 1)
+                 :siblings (fnext board) } )))))
 
 ;; Generates all successor games, ignoring duplicate states
 ;; correctly sets the value of sibling-games to the number
 ;; of other states that were generated to the same square
 (defn expand-game [game]
-  (let ((boards (successor-states (game-board game))))
-    (loop for board in boards collect
-      { :name (game-name game)
-        :board (car board) :parent game
-        :depth (+ (game-depth game) 1)
-        :siblings (cadr board) } ))))
+  (let [boards (successor-states game)]
+    (for [board boards]
+      { :name (:name game)
+        :board (first board) :parent game
+        :depth (+ (:depth game) 1)
+        :siblings (fnext board) } )))
 
 ;; The main search function the arguements are:
 ;; game - Start state (has to be a game class)
@@ -118,29 +111,26 @@
 ;; depthlimit - Depth limit (I never used it for sudoku)
 ;; reps_allowed - controls if duplicate state checking is on or off
 (defn general-search [game q-fn depthlimit & reps_allowed]
-  (let ((closed (make-hash-table :test #'equal))
-    (goal nil) (open (list game)) (to_expand nil) (num_exp 0)
-    (new_nodes nil) (num_gen 0) (start-time (get-internal-run-time)))
-    (while (and (not goal) open) do
-      (setf to-expand (pop open))
-      (incf num_exp)
+  (let [closed (hash-map :test 'equal) goal nil open (list game) to_expand nil num_exp 0
+        new_nodes nil num_gen 0]
+    (while (and (not goal) open) (do
+      (def to-expand (pop open))
+      (set num_exp (+ num_exp 1))
       ;(print open)
       ;(print to-expand)
       (cond
-       ((null to-expand) (return-from basic-search open))
-       ((is-full (game-board to-expand)) (setf goal to-expand))
-       ((and depthlimit (> (game-depth to-expand) depthlimit)) t)
-       (reps_allowed (do
-               (setf new_nodes (expand-game to-expand))
-               (setf num_gen (+ num_gen (length new_nodes)))
-               (setf open (funcall q-fn open new_nodes))))
-       (t (do
-        (setf new_nodes (expand-game-best to-expand closed))
-        (setf num_gen (+ num_gen (length new_nodes)))
-        (setf open (funcall q-fn open new_nodes)))))
-      (if (> num_exp expand_limit) (setf goal "EXCEEDED MAX NODES"))
-      (if (> (- (get-internal-run-time) start-time) max-runtime)
-          (setf goal "EXCEEDED MAX TIME")))
+       (nil? to-expand) (set goal false)
+       (is-full to-expand) (set goal to-expand)
+       (and depthlimit (> (:depth to-expand) depthlimit)) true
+       reps_allowed (do
+               (set new_nodes (expand-game to-expand))
+               (set num_gen (+ num_gen (count new_nodes)))
+               (set open (q-fn open new_nodes)))
+       true (do
+        (set new_nodes (expand-game-best to-expand closed))
+        (set num_gen (+ num_gen (count new_nodes)))
+        (set open (q-fn open new_nodes))))
+      (if (> num_exp expand_limit) (set goal "EXCEEDED MAX NODES"))))
     (println "NUM. EXPANDED")
     (println num_exp)
     (println "NUM. GENERATED")
@@ -148,39 +138,36 @@
     goal))
 
 ;; BFS Queueing function, (removes nil states if they exist)
-(defn q-bfs (old new)
-  (remove-if-not (lambda (x) x)
-         (append old new)))
+(defn q-bfs [old new]
+  (remove nil? (conj old new)))
 ;; DFS Queueing function, (also removes any nil states)
-(defn q-dfs (old new)
-  (remove-if-not (lambda (x) x)
-         (append new old)))
+(defn q-dfs [old new]
+  (remove nil? (cons new old)))
 
 ;; Helper function for BESTFS
 ;; inserts a game into old-games maintaining ascending order of
 ;; (sibling-games i)
-(defn insert-game (game old-games)
+(defn insert-game [game old-games]
   (cond
-   ((null old-games) (list game))
-   ((<= (sibling-games game) (sibling-games (car old-games)))
-    (cons game old-games))
-   (t (cons (car old-games) (insert-game game (cdr old-games))))))
+   (nil? old-games) (list game)
+   (<= (:siblings game) (:siblings (first old-games))) (cons game old-games)
+   true (cons (first old-games) (insert-game game (rest old-games)))))
 
 
 ;; Another helper function for BESTFS
 ;; same as insert-game, except it doesn't fill up the stack for
 ;; large problems. (I also tried a tail recursive version, but the loop
 ;; was faster)
-(defn insert-game-loop (new-game old-games)
-  (let [old-game nil remaining old-games num-old (length old-games)]
-    (if (null old-games) (return-from insert-game-loop (list new-game)))
+(defn insert-game-loop [new-game old-games]
+  (let [old-game nil remaining old-games num-old (count old-games)]
+    (if (nil? old-games) (return-from insert-game-loop (list new-game)))
     (for [i (range 0 num-old)]
-      (setf old-game (car remaining))
-      (if (<= (sibling-games new-game) (sibling-games old-game))
+      (set old-game (car remaining))
+      (if (<= (:siblings new-game) (:siblings old-game))
         (return-from insert-game-loop
           (append (butlast old-games (- num-old i))
           (cons new-game (last old-games (- num-old i))))))
-      (setf remaining (cdr remaining)))
+      (set remaining (rest remaining)))
     (return-from insert-game-loop
       (append old-games (list new-game)))))
 
@@ -196,7 +183,7 @@
   (let ((sorted old))
     (for [game new]
       (if game
-        (setf sorted (insert-game-loop game sorted))))
+        (set sorted (insert-game-loop game sorted))))
     sorted))
 
 ;; BEAM search
@@ -218,8 +205,8 @@
     (for [game new]
       (if game
         (do
-        (setf templist (insert-game-loop game sorted))
-        (setf sorted
+        (set templist (insert-game-loop game sorted))
+        (set sorted
         (subseq templist 0
         (min (length templist) (max (sibling-games (car templist)) 1)))))))
     (append sorted old)))
@@ -227,9 +214,9 @@
 ;; A wrapper to take time stats on the general-search function
 (defn search-stats (game q-fs depthlimit &optional (reps_allowed nil))
   (let [start 0 end 0]
-    (setf start (get-internal-run-time))
-    (setf results (general-search game q-fs depthlimit reps_allowed))
-    (setf end (get-internal-run-time))
+    (set start (get-internal-run-time))
+    (set results (general-search game q-fs depthlimit reps_allowed))
+    (set end (get-internal-run-time))
     (print "RUNTIME")
     (print (- end start))
     results))
